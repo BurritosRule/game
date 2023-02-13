f82637e17e0572929bd7f4e28739d4be1798221c
BurritosRule <code@shrekmail.com>
2023-02-13 15:45:21 -0500

    trying jline3

--- src/main/java/com/github/game/run/Run.java
+++ src/main/java/com/github/game/run/Run.java
@@ -6,6 +6,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReader.Option;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.utils.Log;
+
 import com.github.game.menu.Menu;
 import com.github.game.menu.MenuFactory;
 import com.github.game.player.Player;
@@ -19,19 +29,27 @@ public class Run {
 		
 		TowerImpl castleTower = new TowerImpl("Castle Tower", 10);
 		Player player = new PlayerImpl("Hero", castleTower);
+		
+		
 
-		try (Scanner input = new Scanner(System.in)) {
+        Terminal terminal = TerminalBuilder.builder().build();
 
-			String menuSelection = null;
-			List<Action> possibleInput = null;
-
-			MenuFactory menuFactory = new MenuFactory();
-			
-			Menu menu = menuFactory.getMenu(player.getCurrentLocation());
-
-			while (true) {
-
-				possibleInput = menu.possibleInput();
+        DefaultParser parser = new DefaultParser();
+        LineReader reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .parser(parser)
+                .build();
+        while (true) {
+            try {
+                
+    			String menuSelection = null;
+    			List<Action> possibleInput = null;
+    
+    			MenuFactory menuFactory = new MenuFactory();
+    			
+    			Menu menu = menuFactory.getMenu(player.getCurrentLocation());
+    			
+    			possibleInput = menu.possibleInput();
 				//header = menu.header();
 				StringBuilder keywords = new StringBuilder();
 
@@ -41,19 +59,68 @@ public class Run {
 					actions.put(action.getKeyword(), action);
 				}
 
-				System.out.println("Your current location is floor " + castleTower.getCurrentFloor() + " of "
+				reader.getTerminal().writer().println(menu.header());
+				reader.getTerminal().writer().println("Your current location is floor " + castleTower.getCurrentFloor() + " of "
 						+ castleTower.getName() + "\n");
+				reader.getTerminal().writer().println("\n" + keywords);
 
-				System.out.print(menu.header());
-				
-				System.out.print("\n" + keywords);
-
-				menuSelection = input.next().toLowerCase();
-
+				menuSelection = reader.readLine("Action > ");
 				actions.get(menuSelection).execute();
+                
+                //reader.callWidget(LineReader.CLEAR);
+                //reader.getTerminal().writer().println(line);
+                //reader.callWidget(LineReader.REDRAW_LINE);
+                //reader.callWidget(LineReader.REDISPLAY);
+                //reader.getTerminal().writer().flush();
+                
+                //System.out.println(line);
+            } catch (UserInterruptException e) {
+                // Ignore
+            } catch (EndOfFileException e) {
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+	    
+		
 
-			}
+		
 
-		}
+//		try (Scanner input = new Scanner(System.in)) {
+//
+//			String menuSelection = null;
+//			List<Action> possibleInput = null;
+//
+//			MenuFactory menuFactory = new MenuFactory();
+//			
+//			Menu menu = menuFactory.getMenu(player.getCurrentLocation());
+//
+//			while (true) {
+//
+//				possibleInput = menu.possibleInput();
+//				//header = menu.header();
+//				StringBuilder keywords = new StringBuilder();
+//
+//				Map<String, Action> actions = new HashMap<>();
+//				for (Action action : possibleInput) {
+//					keywords.append(action.getKeyword()).append("\n");
+//					actions.put(action.getKeyword(), action);
+//				}
+//
+//				System.out.println("Your current location is floor " + castleTower.getCurrentFloor() + " of "
+//						+ castleTower.getName() + "\n");
+//
+//				System.out.print(menu.header());
+//				
+//				System.out.print("\n" + keywords);
+//
+//				menuSelection = input.next().toLowerCase();
+//
+//				actions.get(menuSelection).execute();
+//
+//			}
+//
+//		}
 	}
 }
